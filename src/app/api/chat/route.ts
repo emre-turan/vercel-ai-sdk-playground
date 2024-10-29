016ff1da3a671bc3638750faec78276b7fdb2c4a
Emre Turan <itsemreturan@gmail.com>
2024-10-29 22:39:18 +0300

    simple tool usage and it's implementation.

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,5 +1,6 @@
 import { openai } from "@ai-sdk/openai";
-import { streamText } from "ai";
+import { streamText, tool } from "ai";
+import { z } from "zod";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -10,6 +11,35 @@ export async function POST(req: Request) {
   const result = await streamText({
     model: openai("gpt-4o-mini-2024-07-18"),
     messages,
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location (farenheit)",
+        parameters: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+      convertFarenheitToCelsius: tool({
+        description: "Convert a temperature in farenheit to celsius",
+        parameters: z.object({
+          temperature: z
+            .number()
+            .describe("The temperature in farenheit to convert"),
+        }),
+        execute: async ({ temperature }) => {
+          const celsius = Math.round((temperature - 32) * (5 / 9));
+          return {
+            celsius,
+          };
+        },
+      }),
+    },
   });
 
   return result.toDataStreamResponse();
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,20 @@
 import { useChat } from "ai/react";
 
 export default function Chat() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    maxSteps: 5,
+  });
 
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
       {messages.map((m) => (
         <div key={m.id} className="whitespace-pre-wrap">
           {m.role === "user" ? "User: " : "AI: "}
-          {m.content}
+          {m.toolInvocations ? (
+            <pre>{JSON.stringify(m.toolInvocations, null, 2)}</pre>
+          ) : (
+            <p>{m.content}</p>
+          )}
         </div>
       ))}
 
